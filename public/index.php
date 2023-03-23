3c2307da62c79f34e2c0656cc2e656e1dcb310b5
rene.reimann <rene.reimann@immoware24.de>
2023-03-23 07:28:50 +0100

    add app

---
+++ public/index.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace DerPixler\SymfonyRestApi;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use DerPixler\SymfonyRestApi\Controllers\RestController;
+use Exception;
+
+use Symfony\Component\
+{
+    HttpFoundation\Request,
+    HttpFoundation\Response,
+
+    Routing\Matcher\UrlMatcher,
+    Routing\Route,
+    Routing\RouteCollection,
+    Routing\RequestContext,
+
+    HttpKernel\Controller\ArgumentResolver,
+    HttpKernel\Controller\ControllerResolver,
+};
+
+$route = new Route(
+    '/rest-api/{client}/{action}',
+    [
+        '_controller' => RestController::class,
+        'client' => '',
+        'action' => ''
+    ]
+);
+$routes = new RouteCollection();
+$routes->add('api', $route);
+
+$request = Request::createFromGlobals();
+$context = new RequestContext();
+$context->fromRequest($request);
+
+$matcher = new UrlMatcher($routes, $context);
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
+} catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
+}
+
+$response->send();
